fc0d10af7b457d341d5c54ae8eb237f06e528427
showkatbinidris70 <showkatbinidris70@gmail.com>
2023-01-17 10:54:55 +0600

    anisul islam video no 9 access data using for loop and map

--- rapp/src/App.js
+++ rapp/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import CardComponent from "./components/CardComponent";
+import Data from "./data.json";
 
 function App() {
     const appName = 'Todo App';
@@ -8,14 +9,23 @@ function App() {
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
     const contactDate = day + "-" + "0" + month + "-" + year;
+
+    let items = [];
+    //using for loop
+    // for (let i = 0; i < Data.length; i++) {
+    //     items.push(<CardComponent title={Data[i].title} desc={Data[i].desc} mobile={Data[i].mobile}/>)
+    // }
+
+    //using map
+    // items = Data.map((item, index) => <CardComponent key={index} title={item.title} desc={item.desc} mobile={item.mobile}/>);
+
     return (
         <div>
             <h1 className="appName">{appName}</h1>
             <div className="components">
-                <CardComponent title="Call Family" desc="This is family mobile number" mobile="01915923397" date={contactDate}/>
-                <CardComponent title="Call Mother" desc="This is mother's mobile number" mobile="01815923396" date={contactDate}/>
-                <CardComponent title="Call Father" desc="This is father's mobile number" mobile="01715923392" date={contactDate}/>
-                <CardComponent title="Call Sister" desc="This is sister's mobile number" mobile="01515923390" date={contactDate}/>
+                {/*calling array */}
+                {/*{items}*/}
+                {Data.map((item, index) => <CardComponent key={index} title={item.title} desc={item.desc} mobile={item.mobile}/>)}
             </div>
         </div>
     );
