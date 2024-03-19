5ae51b61a1c3c3b67d2ece0b73d33036abac475c
showkatbinidris70 <showkatbinidris70@gmailcom>
2024-03-20 00:01:27 +0600

    sss

--- video11/src/App.js
+++ video11/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import NestedMapping from './components/NestedMapping';
+import logo from "./logo.svg";
+import "./App.css";
+import NestedMapping from "./components/NestedMapping";
 
 function App() {
   return (
     <div>
-      <NestedMapping/>
+      <NestedMapping />
     </div>
   );
 }
--- video11/src/components/NestedMapping.js
+++ video11/src/components/NestedMapping.js
@@ -1,29 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Users from "./users.json";
 
 function NestedMapping() {
+  const [count, setCount] = useState(0);
+  const hangleIncrement = () => {
+    setCount((count) => count + 1);
+  };
   return (
-        <div>
-            {Users.map((user,index) => {
-                const { fullName, age } = user;
-                return (
-                    <div key={index}>
-                        <h2>{fullName}</h2>
-                        <p>{age}</p>
-                        {user.phones.map((phone, index)=>{
-                            const {home, office} = phone;
-                            return(
-                                <div key={index}>
-                                    <p>{home}</p>
-                                    <p>{office}</p>
-                                </div>
-                            );
-                        })}
-                    </div>
-                );
-
+    <div>
+      <h1>Initial Value : {count}</h1>
+      <button onClick={hangleIncrement}>increment</button>
+      {Users.map((user, index) => {
+        const { fullName, age } = user;
+        return (
+          <div>
+            <p>{fullName}</p>
+            <p>{age}</p>
+            {user.phones.map((phone, index) => {
+              return (
+                <div>
+                  <p>{phone.home}</p>
+                  <p>{phone.office}</p>
+                </div>
+              );
             })}
-        </div>
-    );
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 export default NestedMapping;
